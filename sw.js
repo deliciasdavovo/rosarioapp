780d123f54a3d090128d6a3e4bcd1ac0bd4e8c03
deliciasdavovo <laismonteiroe@gmail.com>
2026-08-16 03:33:29 -0300

    Atualiza cache do formato do fruto

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'meu-terco-v17';
+const CACHE = 'meu-terco-v18';
 const PRECACHE = ['./', './index.html', './manifest.json', './icon.svg', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
