70928ddad80e894875768a6a97c5a9b064895af3
deliciasdavovo <laismonteiroe@gmail.com>
2026-08-16 03:24:30 -0300

    Atualiza cache dos ajustes do rosário

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'meu-terco-v16';
+const CACHE = 'meu-terco-v17';
 const PRECACHE = ['./', './index.html', './manifest.json', './icon.svg', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
